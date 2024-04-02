0c08859bf6437924b80f6b38183efe7e388f3687
Felix Corke <felix@blackspike.com>
2024-04-02 16:05:04 +0100

    added mux video

--- netlify/functions/test-function/test-function.js
+++ netlify/functions/test-function/test-function.js
@@ -1,12 +1,27 @@
 import Mux from '@mux/mux-node'
+const mux = new Mux({
+  tokenId: process.env.MUX_TOKEN_ID,
+  tokenSecret: process.env.MUX_TOKEN_SECRET
+});
+
+const CORS_HEADERS = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers':
+    'Origin, X-Requested-With, Content-Type, Accept',
+}
 
 export async function handler(event, context) {
   try {
 
+    const asset = await mux.video.assets.create({
+      input: [{ url: 'https://mux-video-test.netlify.app/animation.mp4' }],
+      playback_policy: ['public'],
+    });
 
     return {
+      headers: CORS_HEADERS,
       statusCode: 200,
-      body: JSON.stringify({ test: "name" }),
+      body: JSON.stringify({ test: "name", asset }),
     }
   } catch (error) {
     return { statusCode: 500, body: error.toString() }
