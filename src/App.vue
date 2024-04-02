76c3aa6a24f4fd26590336860aa426f69c7c5af8
Felix Corke <felix@blackspike.com>
2024-04-02 15:57:43 +0100

    initial commit!

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const output = ref('Pending…')
+
+
+onMounted(async() => {
+  const searchParams = new URLSearchParams(window.location.search)
+  const test = searchParams.get('test')
+
+  const response = await fetch(`/api/test-function?test=${test}`)
+  output.value = await response.json()
+})
+
+</script>
+
+<template>
+
+  <h1>Output:</h1>
+  <pre style="font-size: 30px;">{{ output.fish }}</pre>
+</template>
