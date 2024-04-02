6d79e38b54486015b15ccc68fc8fd25b0a5cfd7e
Felix Corke <felix@blackspike.com>
2024-04-02 16:10:11 +0100

    real token

--- netlify/functions/test-function/test-function.js
+++ netlify/functions/test-function/test-function.js
@@ -4,24 +4,20 @@ const mux = new Mux({
   tokenSecret: process.env.MUX_TOKEN_SECRET
 });
 
-const CORS_HEADERS = {
-  'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Headers':
-    'Origin, X-Requested-With, Content-Type, Accept',
-}
-
 export async function handler(event, context) {
   try {
 
-    const asset = await mux.video.assets.create({
-      input: [{ url: 'https://mux-video-test.netlify.app/animation.mp4' }],
-      playback_policy: ['public'],
-    });
+    let baseOptions = {
+      keyId: process.env.muxSigningKeyId,
+      keySecret: process.env.muxSigningKeySecret,
+      expiration: '2d', // E.g 60, "2 days", "10h", "7d", numeric value interpreted as seconds
+    }
+
+    const token = await mux.jwt.signPlaybackId(playbackId, { ...baseOptions, type: 'video' })
 
     return {
-      headers: CORS_HEADERS,
       statusCode: 200,
-      body: JSON.stringify({ test: "name", asset }),
+      body: JSON.stringify({ test: "name", token }),
     }
   } catch (error) {
     return { statusCode: 500, body: error.toString() }
